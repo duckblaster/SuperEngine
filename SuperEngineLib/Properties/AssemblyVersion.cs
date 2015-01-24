ab0409b052829954046aea7430a0a955acf0a0f7
Donald Duncan <duckblaster7090@gmail.com>
2015-01-25 00:27:41 +1300

    Added missing AssemblyVersion.cs files

---
+++ SuperEngineLib/Properties/AssemblyVersion.cs
@@ -0,0 +1,5 @@
+/* UpdateVersion Revision: 0 */
+using System.Reflection;
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.12.0")]
+[assembly: AssemblyInformationalVersion("csharp-v1.0-12-gc924942-0")]
