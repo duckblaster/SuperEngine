73d6d30ac2f071b16f1280504e06b737fa3de4e9
Donald Duncan <duckblaster7090@gmail.com>
2015-01-26 11:43:54 +1300

    Switch from xUnit to nUnit

--- SuperEngine.Tests/Tests.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using Xunit;
-
-namespace SuperEngine.Tests {
-    public class Tests {
-    }
-}
---
+++ SuperEngineTests/DebugGUITests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SuperEngine;
+using NUnit.Framework;
+namespace SuperEngine.Tests {
+    [TestFixture()]
+    public class DebugGUITests {
+    }
+}
